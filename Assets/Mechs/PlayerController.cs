bd6d18e59bc599de0d7286679fc1359854788603
JC <joelchanxy@gmail.com>
2023-06-23 11:42:42 +0930

    Missing script ref, fixed movement

--- Assets/Mechs/PlayerController.cs
+++ Assets/Mechs/PlayerController.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public CharacterController controller; // Have to set in inspector
+    public CharacterController controller; // Set in inspector
 
-    public float speed = 12f; // TODO: directional speed
-    public float gravity = -9.81f;
+    public float speed = 12f;
+    public float gravity = -9.81f; // Note we're using negative gravity
     public float jumpHeight = 3f;
     public float groundStick = -2f;
 
+    // Assuming this is the main camera, attach to player camera in the inspector
+    public Transform cam;
+
     // Mouselogic
     public float mouseSensitivity = 300f;
-    public Transform playerBody; // Note this has to be set in the inspector
     public float xRotation = 0f;
 
     // Ground collision logic
@@ -27,11 +29,9 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Starting");
-        // Lock cursor to centre
         Cursor.lockState = CursorLockMode.Locked;
-        // Set initial rotation for cam predictability
-        transform.Rotate(180, 0, 0); //TODO make it forwards
+        // This line will set the camera rotation to the "default" upright position
+        cam.localRotation = Quaternion.identity;
     }
 
     // Update is called once per frame
@@ -40,38 +40,40 @@ public class PlayerController : MonoBehaviour
         //Section 1: Mouse/Camera input
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-        transform.Rotate(Vector3.up, mouseX, Space.World); // Handles horizontal rotation
-        transform.Rotate(Vector3.left, mouseY, Space.Self); // Handles vertical rotation
+
+        // Rotate the entire player on horizontal axis
+        transform.Rotate(Vector3.up * mouseX); // Horizontal rotation
+
+        // Rotate only the camera on vertical axis
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        cam.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
         // Section 2: Movement input
-        // Note: inputs may need to be configured in (Edit->Project Settings->Input manager)
-        // Vertical movement/physics
-        // TODO Debug grounding disabling jump, also look into refactor this section:
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        if (isGrounded && velocity.y < 0)
-        {
-            // <0 check stops clash with jumping
-            // Apparently forcing player to the ground is better
-            velocity.y = -2f;
-        }
-        else
-        {
-            velocity.y += gravity * Time.deltaTime;
-        }
-
-        // Jump equation
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * groundStick * gravity);
-        }
-
-        controller.Move(velocity * Time.deltaTime);
-
-        // Horizontal plane
+        // Gather input for horizontal (side/forward) movement
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = (transform.right * x) + (transform.forward * z);
-        controller.Move(move * speed * Time.deltaTime);
+
+        // Jump
+        if (controller.isGrounded)
+        {
+            if (velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+        }
+
+        // Apply gravity
+        velocity.y += gravity * Time.deltaTime;
+
+        // Apply movement and gravity
+        controller.Move(move * speed * Time.deltaTime + velocity * Time.deltaTime);
     }
 }
