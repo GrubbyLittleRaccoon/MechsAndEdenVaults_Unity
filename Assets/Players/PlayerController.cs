38acf6776cf57acc718d2b4c2bbd8659298369db
GrubbyLittleRaccoon <joelchanxy@gmail.com>
2023-06-25 14:19:50 +0930

    misc cleanup

--- Assets/Players/PlayerController.cs
+++ Assets/Players/PlayerController.cs
@@ -9,10 +9,10 @@ public class PlayerController : MonoBehaviour
 
     public float speed = 12f;
     public float gravity = -9.81f; // Note we're using negative gravity
-    public float jumpHeight = 3f;
+    public float jumpHeight = 0.5f;
     public float groundStick = -2f;
 
-    // Assuming this is the main camera, attach to player camera in the inspector
+    // Attach to player camera in the inspector
     public Transform cam;
 
     // Mouselogic
@@ -31,91 +31,67 @@ public class PlayerController : MonoBehaviour
     //PlayerControls playerControls; // Generated from player controls asset
     PlayerInput playerInput;
     Vector2 move;
-    Vector2 rotate;
+    Vector2 look;
 
     // As the game starts, before Start()
     void Awake()
     {
-        /*
-        playerControls = new PlayerControls();
-
-        // Pulling values defined in inputactions component
-        playerControls.Player.Movement.performed += ctx => move = ctx.ReadValue<Vector2>();
-        playerControls.Player.Movement.canceled += ctx => move = Vector2.zero;
-        playerControls.Player.Camera.performed += ctx => rotate = ctx.ReadValue<Vector2>();
-        playerControls.Player.Camera.canceled += ctx => rotate = Vector2.zero;
-        */
-
-        playerInput = GetComponent<PlayerInput>();
-        Debug.Log("playerInput: " + playerInput);
         // Link actions to methods
+        playerInput = GetComponent<PlayerInput>();
         playerInput.actions["Movement"].performed += ctx => OnMove(ctx.ReadValue<Vector2>());
         playerInput.actions["Movement"].canceled += ctx => OnMove(Vector2.zero);
-
         playerInput.actions["Camera"].performed += ctx => OnLook(ctx.ReadValue<Vector2>());
         playerInput.actions["Camera"].canceled += ctx => OnLook(Vector2.zero);
-
         playerInput.actions["Jump"].performed += ctx => OnJump();
 
-
         Cursor.lockState = CursorLockMode.Locked;
-        cam.localRotation = Quaternion.identity;
+        cam.localRotation = Quaternion.identity; //Zero initial rotation
+    }
+
+    private void OnMove(Vector2 moveValue)
+    {
+        move = moveValue;
+    }
+
+    private void OnLook(Vector2 lookValue)
+    {
+        look = lookValue;
+    }
+
+    private void OnJump()
+    {
+        if (controller.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Section 1: /Camera input
-        float mouseX = rotate.x * mouseSensitivity * Time.deltaTime;
-        float mouseY = rotate.y * mouseSensitivity * Time.deltaTime;
-
+        //Section 1: Camera input
+        float lookDeltaX = look.x * mouseSensitivity * Time.deltaTime;
+        float lookDeltaY = look.y * mouseSensitivity * Time.deltaTime;
         // Rotate the entire player on horizontal axis
-        transform.Rotate(Vector3.up * mouseX); // Horizontal rotation
-
+        transform.Rotate(Vector3.up * lookDeltaX);
         // Rotate only the camera on vertical axis
-        xRotation -= mouseY;
+        xRotation -= lookDeltaY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         cam.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
         // Section 2: Movement input
-        Vector3 movement = new Vector3(move.x, 0, move.y);
-        movement = transform.TransformDirection(movement); //???
+        Vector3 moveVector = new Vector3(move.x, 0, move.y);
+        // Transform the moveVector to the camera's direction instead of the player body's direction (tank-style movement)
+        moveVector = transform.TransformDirection(moveVector);
 
-        // Jump
-        if (controller.isGrounded)
+        // Section 3: Gravity
+        velocity.y += gravity * Time.deltaTime; //General gravity
+        if (controller.isGrounded && velocity.y < 0)
         {
-            if (velocity.y < 0)
-            {
-                velocity.y = -2f;
-            }
+            velocity.y = -2f; // Avoid gliding/sinking from calculation issues
         }
 
-        // Apply gravity
-        velocity.y += gravity * Time.deltaTime;
-
-        // Apply movement and gravity
-        controller.Move(movement * speed * Time.deltaTime + velocity * Time.deltaTime);
-    }
-
-    private void OnMove(Vector2 moveValue)
-    {
-        Debug.Log("OnMove");
-        move = moveValue;
-    }
-
-    private void OnLook(Vector2 rotateValue)
-    {
-        Debug.Log("OnLook");
-        rotate = rotateValue;
-    }
-
-    private void OnJump()
-    {
-        Debug.Log("OnJump");
-
-        if (controller.isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
+        // Apply movement
+        controller.Move(moveVector * speed * Time.deltaTime + velocity * Time.deltaTime);
     }
 }
