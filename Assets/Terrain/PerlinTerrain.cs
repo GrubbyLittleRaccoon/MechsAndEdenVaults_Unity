06795ddb8cbf6b3eb8fae1cff47619e0975c4ed3
GrubbyLittleRaccoon <joelchanxy@gmail.com>
2023-07-04 00:38:36 +0930

    added shader intersect, turned off rippling

--- Assets/Terrain/PerlinTerrain.cs
+++ Assets/Terrain/PerlinTerrain.cs
@@ -6,7 +6,7 @@ public class PerlinTerrain : MonoBehaviour
 {
     // Area settings
     public int width = 256;
-    public int height = 256;
+    public int length = 256;
 
     // Noise settings
     public int octaves = 5; // Number of perlin octaves. Combining perlin layers is actually fractional brownian motion but whatever
@@ -26,12 +26,16 @@ public class PerlinTerrain : MonoBehaviour
     private Terrain terrain;
 
     // Limit the settings
-    private void OnValidate() {
+    private void OnValidate()
+    {
     }
-    
-    private void Start() {
+
+    private void Start()
+    {
+        // Preempetively calculate the total terrain height for alignment purposes
         netAmp = 0;
-        for (int i = 0; i < octaves; i++) {
+        for (int i = 0; i < octaves; i++)
+        {
             netAmp += baseAmp * Mathf.Pow(scaleAmp, i);
         }
 
@@ -40,15 +44,17 @@ public class PerlinTerrain : MonoBehaviour
         terrain.terrainData = GenerateTerrain(terrain.terrainData);
     }
 
-    void Update(){
+    void Update()
+    {
         // Add movement for testing
         //offsetX += Time.deltaTime * 0.5f;
         //terrain.terrainData = GenerateTerrain(terrain.terrainData);
     }
 
-    TerrainData GenerateTerrain (TerrainData terrainData) {
-        terrainData.heightmapResolution = width+1;
-        terrainData.size = new Vector3(width, baseAmp*netAmp, height); // Sets the dimensions of the terrain
+    TerrainData GenerateTerrain(TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = width + 1;
+        terrainData.size = new Vector3(width, baseAmp * netAmp, length); // Sets the dimensions of the terrain
         terrainData.SetHeights(0, 0, GenerateHeights()); //0,0 is the starting point
 
         return terrainData;
@@ -57,12 +63,16 @@ public class PerlinTerrain : MonoBehaviour
     /**
      * Generate a brownian [width, height] array that contains series of heights between 0 and 1
      */
-    float[,] GenerateHeights() {
-        float[,] heights = new float[width, height];
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
+    float[,] GenerateHeights()
+    {
+        float[,] heights = new float[width, length];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < length; y++)
+            {
                 heights[x, y] = 0;
-                for (int i=0; i< octaves; i++) {
+                for (int i = 0; i < octaves; i++)
+                {
                     float iterAmp = Mathf.Pow(scaleAmp, i);
                     float iterScale = Mathf.Pow(scaleFreq, i);
                     heights[x, y] += (CalculateOffsetHeight(x, y, iterScale) * iterAmp);
@@ -76,22 +86,25 @@ public class PerlinTerrain : MonoBehaviour
     /**
      * Calculate perlin offset height for a single x,y point.
      */
-    float CalculateHeight(int x, int y, float scale) {
+    float CalculateHeight(int x, int y, float scale)
+    {
         float xCoord = (((float)x / width) * scale) + offsetX;
-        float yCoord = (((float)y / height) * scale) + offsetY;
+        float yCoord = (((float)y / length) * scale) + offsetY;
         return Mathf.PerlinNoise(xCoord, yCoord);
     }
 
     /**
      * Calculate perlin offset height for a single x,y point.
      * Adds perlin offset to for structured noise (See https://www.youtube.com/watch?v=lctXaT9pxA0)
+     * Returns a value between 0 and "scale
      */
-    float CalculateOffsetHeight(int x, int y, float scale) {
+    float CalculateOffsetHeight(int x, int y, float scale)
+    {
         // Factor in x/y configuration
-        float xCoord = (((float)x / width) * scale ) + offsetX;
-        float yCoord = (((float)y / height) * scale )+ offsetY;
+        float xCoord = (((float)x / width) * scale) + offsetX;
+        float yCoord = (((float)y / length) * scale) + offsetY;
 
-        float noiseValue = Mathf.PerlinNoise(xCoord, yCoord)-0.5f; // Returns value between -0.5 to 0.5
+        float noiseValue = Mathf.PerlinNoise(xCoord, yCoord) - 0.5f; // Returns value between -0.5 to 0.5
 
         // Consider playing with structured offset noise at some point
         float noisyOffsetX = xCoord - (noiseValue * offsetScale);
@@ -101,9 +114,11 @@ public class PerlinTerrain : MonoBehaviour
     }
 }
 
+// Consider mixed 2D texture procedural mix
 // Ep4: Color https://www.youtube.com/watch?v=RDQK1_SWFuc&list=PLFt_AvWsXl0eBW2EiBtl_sxmDtSgZBxB3&index=4
 [System.Serializable]
-public struct TerrainType {
+public struct TerrainType
+{
     public string name;
     public float height;
     public Color color;
